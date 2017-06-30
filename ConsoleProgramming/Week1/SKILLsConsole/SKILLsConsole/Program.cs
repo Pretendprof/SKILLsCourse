04cfe7f4dacefbe3ca0bda0821015d2e19ed32f2
Maurice Lamb <maurice.lamb@uc.edu>
2017-06-30 12:48:52 -0400

    Initial Commit. Add week 1 Files.

---
+++ ConsoleProgramming/Week1/SKILLsConsole/SKILLsConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; // Using declaration at top of script identifies libraries (Identified by a namespaces) used in script
+
+namespace SKILLsConsole // useful for keeping track of/organizing class libraries
+{
+    class Program // this is a class It is an object that groups variables, types, methods and events
+    {
+       
+        static void Main(string[] args) //This is the Main method. It is the first step into the program and thus can be thought of as the program controller or outline
+        {
+            Console.WriteLine("Hello World!"); // Writes string in () to the console
+
+            //HelloWorld(); // calls a hello World Method 
+            //Console.WriteLine(HelloStringOuput()); // calls method that outputs hello world string to console.writeline command
+
+            //ConsoleParrot.RunParrot(); // calls the console parrot program from homework
+            
+            //Console.WriteLine("\nPress any key to continue...\n"); // \n in a string tells program to write a new line 
+            //Console.ReadKey(); //waits for a key press before continuing the program 
+        }
+
+        static void HelloWorld() //Runs Hello world as seperate method
+        {
+            Console.WriteLine("Hello World!"); // same code as above for writing to console
+        }
+
+        static string HelloStringOuput() // generates text for hello mwthod with string method not it says "string" not "void" for its return type
+        {
+            string temp = "hello World!"; // sets value for string
+
+            return temp; // returns string (this is the ouput of this method)
+        }
+    }
+}
